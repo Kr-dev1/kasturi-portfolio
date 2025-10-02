5704a23e4864a2fa229106a1be0bd1ac55cfc8f1
Kasturi Rangan <kasturi.rangan@markany.com>
2025-10-03 01:49:53 +0530

    fixed ts error for deployment

--- src/app/projects/types.ts
+++ src/app/projects/types.ts
@@ -1,16 +1,16 @@
 export interface ProjectCardProps {
-    name: string,
-    description: string,
-    slug: string,
-    tagline: string,
-    selected: boolean,
-    techstack: string[],
-    github: string,
-    homepage: string,
-    githubFrontend: string,
-    githubBackend: string
+  name: string;
+  description: string;
+  slug: string;
+  tagline: string;
+  selected: boolean;
+  techstack: string[];
+  github?: string;
+  homepage?: string;
+  githubFrontend?: string;
+  githubBackend?: string;
 }
 
 export interface ProjectCardsProps {
-    projects: ProjectCardProps[]
+  projects: ProjectCardProps[];
 }
